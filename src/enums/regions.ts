9e8975440e8c7b372a5bff26a2763e0a2efb2566
Demian Rosas <demianrosas@hotmail.com>
2023-05-02 23:39:45 -0300

    feat: add new regions

--- src/enums/regions.ts
+++ src/enums/regions.ts
@@ -2,17 +2,22 @@ export const regionMap = {
   // US
   usCentral1: 'us-central1',               
   usEast1: 'us-east1',                 
-  usEast4: 'us-east4',                 
+  usEast4: 'us-east4',
+  usEast5: 'us-east5',
   usWest1: 'us-west1',                 
   usWest2: 'us-west2',                 
   usWest3: 'us-west3',                 
-  usWest4: 'us-west4',  
+  usWest4: 'us-west4',
+  usSouth1: 'us-south1',
   // Canada
   northAmericaNorthEast1: 'northamerica-northeast1',
+  northamericaNorthEast2: 'northamerica-northeast2',
   // Brazil
   southAmericaEast1: 'southamerica-east1',  
+  southamericaWest1: 'southamerica-west1',
   // Australia
   australiaSoutheast1: 'australia-southeast1',     
+  australiaSoutheast2: 'australia-southeast2',
   // Asia
   asiaEast1: 'asia-east1', 
   asiaEast2: 'asia-east2',
@@ -20,6 +25,7 @@ export const regionMap = {
   asiaNortheast2: 'asia-northeast2',          
   asiaNortheast3: 'asia-northeast3',        
   asiaSouth1: 'asia-south1',              
+  asiaSouth2: 'asia-south2',
   asiaSoutheast1: 'asia-southeast1',         
   asiaSoutheast2: 'asia-southeast2', 
   // Europe
@@ -27,6 +33,13 @@ export const regionMap = {
   europeWest2: 'europe-west2',            
   europeWest3: 'europe-west3',            
   europeWest6: 'europe-west6',    
+  europeWest8: 'europe-west8',
+  europeWest9: 'europe-west9',
+  europeWest12: 'europe-west12',
+  europeSouthwest1: 'europe-southwest1',
+  // MIDDLE EAST
+  meCentral1: 'me-central1',
+  meWest1: 'me-west1'
 }
 
 const {
@@ -34,16 +47,21 @@ const {
   usCentral1,               
   usEast1,                 
   usEast4,                 
+  usEast5,
   usWest1,                 
   usWest2,                 
   usWest3,                 
   usWest4,  
+  usSouth1,
   // Canada
   northAmericaNorthEast1,
+  northamericaNorthEast2,
   // Brazil
   southAmericaEast1,  
+  southamericaWest1,
   // Australia
   australiaSoutheast1,     
+  australiaSoutheast2,
   // Asia
   asiaEast1, 
   asiaEast2,
@@ -51,6 +69,7 @@ const {
   asiaNortheast2,          
   asiaNortheast3,        
   asiaSouth1,              
+  asiaSouth2,
   asiaSoutheast1,         
   asiaSoutheast2, 
   // Europe
@@ -58,6 +77,13 @@ const {
   europeWest2,            
   europeWest3,            
   europeWest6,    
+  europeWest8,
+  europeWest9,
+  europeWest12,
+  europeSouthwest1,
+  // MIDDLE EAST
+  meCentral1,
+  meWest1
 } = regionMap
 
 export default [
@@ -65,16 +91,21 @@ export default [
   usCentral1,               
   usEast1,                 
   usEast4,                 
+  usEast5,
   usWest1,                 
   usWest2,                 
   usWest3,                 
   usWest4,  
+  usSouth1,
   // Canada
   northAmericaNorthEast1,
+  northamericaNorthEast2,
   // Brazil
   southAmericaEast1,  
+  southamericaWest1,
   // Australia
   australiaSoutheast1,     
+  australiaSoutheast2,
   // Asia
   asiaEast1, 
   asiaEast2,
@@ -82,6 +113,7 @@ export default [
   asiaNortheast2,          
   asiaNortheast3,        
   asiaSouth1,              
+  asiaSouth2,
   asiaSoutheast1,         
   asiaSoutheast2, 
   // Europe
@@ -89,4 +121,11 @@ export default [
   europeWest2,            
   europeWest3,            
   europeWest6,    
+  europeWest8,
+  europeWest9,
+  europeWest12,
+  europeSouthwest1,
+  // MIDDLE EAST
+  meCentral1,
+  meWest1
 ]
